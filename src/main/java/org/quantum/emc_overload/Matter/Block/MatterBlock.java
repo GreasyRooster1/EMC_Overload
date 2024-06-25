c3ee3b5e29a94d684a5058d2d853996319c47002
greasyRooster1 <GreasyRooster1@gmail.com>
2024-06-25 01:33:40 -0700

    make flux matter block have texture

--- src/main/java/org/quantum/emc_overload/Matter/Block/MatterBlock.java
+++ src/main/java/org/quantum/emc_overload/Matter/Block/MatterBlock.java
@@ -4,14 +4,27 @@ import moze_intel.projecte.gameObjs.EnumMatterType;
 import moze_intel.projecte.gameObjs.blocks.IMatterBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.quantum.emc_overload.Matter.Builder.MatterTier;
+
+import java.util.function.ToIntFunction;
 
 public class MatterBlock extends Block implements TieredMatterBlock {
     public final MatterTier matterTier;
 
     public MatterBlock(MatterTier tier) {
-        super(BlockBehaviour.Properties.of().strength(3000000.0F+(tier.getProgression()*1000000.0F), 6000000.0F+(tier.getProgression()*1000000.0F)));
+        super(BlockBehaviour.Properties.of()
+                .strength(3000000.0F+(tier.getProgression()*1000000.0F), 6000000.0F+(tier.getProgression()*1000000.0F))
+                .lightLevel(litBlockEmission(15))
+        );
         this.matterTier = tier;
+    }
+
+    private static ToIntFunction<BlockState> litBlockEmission(int p_50760_) {
+        return (p_50763_) -> {
+            return (Boolean)p_50763_.getValue(BlockStateProperties.LIT) ? p_50760_ : 0;
+        };
     }
 
     public MatterTier getMatterTier() {
