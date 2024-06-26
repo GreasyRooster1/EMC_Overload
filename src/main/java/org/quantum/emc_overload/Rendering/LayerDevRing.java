6d98dfeef97d617290b043217e6bcfd8193eb9f1
greasyRooster1 <GreasyRooster1@gmail.com>
2024-06-26 01:06:39 -0700

    added dev ring animation

--- src/main/java/org/quantum/emc_overload/Rendering/LayerDevRing.java
+++ src/main/java/org/quantum/emc_overload/Rendering/LayerDevRing.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
@@ -18,14 +19,20 @@ import org.quantum.emc_overload.EMCOverload;
 
 import java.util.UUID;
 
+import static java.lang.Math.*;
+import static net.minecraft.util.Mth.HALF_PI;
+import static net.minecraft.util.Mth.TWO_PI;
 import static org.quantum.emc_overload.Rendering.ModRenderType.DEV_RING_RENDERER;
 
 public class LayerDevRing extends RenderLayer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> {
     private static final UUID Q_UUID = UUID.fromString("3dcfb3de-80a2-4596-9d82-5bcb5b405676");
     private static final UUID C_UUID = UUID.fromString("ee06c1b0-5520-4cc1-add5-0aeaf95c78a9");
-    private static final ResourceLocation Q1_LOC = EMCOverload.rl("textures/models/quantum1.png");
-    private static final ResourceLocation Q2_LOC = EMCOverload.rl("textures/models/quantum2.png");
-    private static final ResourceLocation C1_LOC = EMCOverload.rl("textures/models/crafter1.png");
+    private static final ResourceLocation QUANTUM1_LOC = EMCOverload.rl("textures/models/quantum1.png");
+    private static final ResourceLocation QUANTUM2_LOC = EMCOverload.rl("textures/models/quantum2.png");
+    private static final ResourceLocation CRAFTER1_LOC = EMCOverload.rl("textures/models/crafter1.png");
+    private static final int[] cColor = {0, 255, 162};
+    private static final int[] qColor = {255, 0, 255};
+    private static final int secondsPerRotation = 18;
     private final PlayerRenderer render;
 
     public LayerDevRing(PlayerRenderer renderer) {
@@ -35,40 +42,50 @@ public class LayerDevRing extends RenderLayer<AbstractClientPlayer, PlayerModel<
 
     public void render(@NotNull PoseStack matrix, @NotNull MultiBufferSource renderer, int light, @NotNull AbstractClientPlayer player, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         if (!player.isInvisible()) {
-            boolean isQ = Q_UUID.equals(player.getUUID())||!FMLEnvironment.production;
-            if (!FMLEnvironment.production || Q_UUID.equals(player.getUUID()) || C_UUID.equals(player.getUUID())) {
-                matrix.pushPose();
-                ((PlayerModel<?>)this.render.getModel()).jacket.translateAndRotate(matrix);
-                double yShift = -0.497;
 
-                if (player.isCrouching()) {
-                    matrix.mulPose(Axis.XP.rotationDegrees(-28.64789F));
-                    yShift = -0.44;
+            boolean isQuantum = Q_UUID.equals(player.getUUID())||!FMLEnvironment.production;
+            boolean isCrafter = C_UUID.equals(player.getUUID());
+
+            if (isQuantum || isCrafter) {
+                float timeVal = player.tickCount%(secondsPerRotation*20);
+                float angle = (timeVal/(secondsPerRotation*20))*360;
+
+                if(isQuantum){
+                    createCircle(angle,0,3f,qColor,QUANTUM1_LOC,matrix,renderer,player.isCrouching());
+                    createCircle(-angle,0,2.5f,qColor,QUANTUM2_LOC,matrix,renderer,player.isCrouching());
                 }
-
-                matrix.mulPose(Axis.ZP.rotationDegrees(180.0F));
-                matrix.scale(3.0F, 3.0F, 3.0F);
-                matrix.translate(-0.5, yShift, -0.5);
-
-                ResourceLocation loc = Q1_LOC;
-
-                if(isQ){
-                    loc = Q1_LOC;
+                if(isCrafter){
+                    createCircle(angle,1.5f,1.1f,cColor,CRAFTER1_LOC,matrix,renderer,player.isCrouching());
                 }
-                if(C_UUID.equals(player.getUUID())){
-                    loc = C1_LOC;
-                }
-
-                VertexConsumer builder = renderer.getBuffer((RenderType) DEV_RING_RENDERER.apply(loc));
-                Matrix4f matrix4f = matrix.last().pose();
-                builder.vertex(matrix4f, 0.0F, 0.0F, 0.0F).color(255, 0, 255, 255).uv(0.0F, 0.0F).endVertex();
-                builder.vertex(matrix4f, 0.0F, 0.0F, 1.0F).color(255, 0, 255, 255).uv(0.0F, 1.0F).endVertex();
-                builder.vertex(matrix4f, 1.0F, 0.0F, 1.0F).color(255, 0, 255, 255).uv(1.0F, 1.0F).endVertex();
-                builder.vertex(matrix4f, 1.0F, 0.0F, 0.0F).color(255, 0, 255, 255).uv(1.0F, 0.0F).endVertex();
-                matrix.popPose();
             }
 
         }
+    }
+
+    private void createCircle(float angle, double y, float scale, int[] color, ResourceLocation loc, PoseStack matrix, MultiBufferSource renderer, boolean isCrouching){
+        matrix.pushPose();
+        ModelPart jacket = this.render.getModel().jacket;
+        jacket.translateAndRotate(matrix);
+        double yShift = -0.497+y;
+
+        if (isCrouching) {
+            matrix.mulPose(Axis.XP.rotationDegrees(-28.64789F));
+            yShift = -0.44+y;
+        }
+
+        matrix.mulPose(Axis.ZP.rotationDegrees(180F));
+        matrix.mulPose(Axis.YP.rotationDegrees(angle- jacket.yRot));
+        matrix.scale(scale, 3, scale);
+        matrix.translate(-0.5, yShift, -0.5);
+
+
+        VertexConsumer builder = renderer.getBuffer(DEV_RING_RENDERER.apply(loc));
+        Matrix4f matrix4f = matrix.last().pose();
+        builder.vertex(matrix4f, 0f,0f,0f).color(color[0], color[1], color[2], 255).uv(0.0F, 0.0F).endVertex();
+        builder.vertex(matrix4f, 0f,0f,1f).color(color[0], color[1], color[2], 255).uv(0.0F, 1.0F).endVertex();
+        builder.vertex(matrix4f, 1f,0f,1f).color(color[0], color[1], color[2], 255).uv(1.0F, 1.0F).endVertex();
+        builder.vertex(matrix4f, 1f,0f,0f).color(color[0], color[1], color[2], 255).uv(1.0F, 0.0F).endVertex();
+        matrix.popPose();
     }
 
 }
